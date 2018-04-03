be090db82a52d559b4fa16b5a863848f65906d21
LarsonYong <jianbo1889@gmail.com>
2018-04-02 22:14:24 -0700

    add

--- react-redux-client/src/containers/Todos.js
+++ react-redux-client/src/containers/Todos.js
@@ -17,7 +17,11 @@ const mapDispatchToProps = (dispatch) => {
     //you can now say this.props.mappedAppActions
     fetchTodos: () => dispatch(todoActions.fetchTodos()),
     mappedDeleteTodo: todoToDelete => dispatch(todoActions.deleteTodo(todoToDelete)),
-    mappedEditTodo: todoToEdit => dispatch(todoActions.editTodo(todoToEdit))
+    mappedEditTodo: todoToEdit => dispatch(todoActions.editTodo(todoToEdit)),
+    mappedshowEditModal: todoToEdit => dispatch(todoActions.showEditModal(todoToEdit)),
+    mappedhideEditModal: () => dispatch(todoActions.hideEditModal()),
+    mappedshowDeleteModal: todoToDelete => dispatch(todoActions.showDeleteModal(todoToDelete)),
+    mappedhideDeleteModal: () => dispatch(todoActions.hideDeleteModal())
   }
 }
 
